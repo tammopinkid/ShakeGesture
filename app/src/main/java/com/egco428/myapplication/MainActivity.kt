6a7513a22e71ca2fd95c11f7116170706422c96b
tammopinkid <tammo.pinkid@gmail.com>
2017-11-09 12:13:38 +0700

    Initial commit

---
+++ app/src/main/java/com/egco428/myapplication/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.egco428.myapplication
+
+import android.content.Context
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    private var sensorManager: SensorManager? = null
+    private var color = false
+    private var view: View? = null
+    private var lastUpdate: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView.setBackgroundColor(Color.GREEN)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lastUpdate = System.currentTimeMillis()
+    }
+
+    private fun getAccelerometer(event: SensorEvent) {
+        val values = event.values
+        // Movement
+        val x = values[0]
+        val y = values[1]
+        val z = values[2]
+
+        val accel = (x * x + y * y + z * z) / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH)
+        val actualTime = System.currentTimeMillis()
+        if (accel >= 2)
+        //
+        {
+            if (actualTime - lastUpdate < 200) {
+                return
+            }
+            lastUpdate = actualTime
+            Toast.makeText(this, "Device was shuffled", Toast.LENGTH_SHORT)
+                    .show()
+            if (color) {
+                textView!!.setBackgroundColor(Color.GREEN)
+
+            } else {
+                textView!!.setBackgroundColor(Color.RED)
+            }
+            color = !color
+        } //else {
+//            Toast.makeText(this, "Device was not shuffled", Toast.LENGTH_SHORT)
+//                    .show()
+//        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            getAccelerometer(event)
+        }
+    }
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        sensorManager!!.registerListener(this,sensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager!!.unregisterListener(this)
+    }
+}
